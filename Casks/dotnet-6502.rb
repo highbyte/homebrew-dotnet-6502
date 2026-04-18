aa6a982ad5ca88d0db1c6d4107c9d30cc8ca9190
Highbyte <highbyte@outlook.com>
2026-04-18 23:48:34 +0200

    Consistent name of the app

--- Casks/dotnet-6502.rb
+++ Casks/dotnet-6502.rb
@@ -4,7 +4,7 @@ cask "dotnet-6502" do
   url "https://github.com/highbyte/dotnet-6502/releases/download/v#{version}/DotNet6502-Avalonia-osx-arm64.zip"
   sha256 "8c1c24ecadbef93149eb78b93875a8b2b66f8920c1fb5a54ddc1f2ec412b79a0"
 
-  name "DotNet6502 Emulator"
+  name "DotNet 6502 Emulator"
   desc "6502 CPU and C64 emulator built with .NET and Avalonia"
   homepage "https://github.com/highbyte/dotnet-6502"
 
