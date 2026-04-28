a346694010f5fa16d5c9d77ea8b06e3674ce0a9f
Highbyte <highbyte@outlook.com>
2026-04-28 17:53:59 +0200

    Add dotnet-6502-remote formula

---
+++ Formula/dotnet-6502-remote.rb
@@ -0,0 +1,38 @@
+class Dotnet6502Remote < Formula
+  desc "Remote control CLI client for the dotnet-6502 emulator"
+  homepage "https://github.com/highbyte/dotnet-6502"
+  version "0.30.0-alpha"
+  license "MIT"
+
+  on_macos do
+    on_arm do
+      url "https://github.com/highbyte/dotnet-6502/releases/download/v#{version}/DotNet6502-RemoteClient-osx-arm64.zip"
+      sha256 "0000000000000000000000000000000000000000000000000000000000000000"
+    end
+  end
+
+  on_linux do
+    on_intel do
+      url "https://github.com/highbyte/dotnet-6502/releases/download/v#{version}/DotNet6502-RemoteClient-linux-x64.zip"
+      sha256 "0000000000000000000000000000000000000000000000000000000000000000"
+    end
+    on_arm do
+      url "https://github.com/highbyte/dotnet-6502/releases/download/v#{version}/DotNet6502-RemoteClient-linux-arm64.zip"
+      sha256 "0000000000000000000000000000000000000000000000000000000000000000"
+    end
+  end
+
+  def install
+    # Install everything into libexec (app needs native libs in same directory)
+    libexec.install Dir["*"]
+    # Shell wrapper that launches the executable from libexec
+    (bin/"dotnet-6502-remote").write <<~EOS
+      #!/bin/bash
+      exec "#{libexec}/Highbyte.DotNet6502.App.RemoteClient" "$@"
+    EOS
+  end
+
+  test do
+    assert_predicate bin/"dotnet-6502-remote", :exist?
+  end
+end
